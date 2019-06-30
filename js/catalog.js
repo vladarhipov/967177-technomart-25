f6f44c9c0ae082568cc5b0f64987b2ccca438fb9
Vlad Arkhipov <info@vladarhipov.ru>
2019-06-30 22:36:25 +0300

    Another Improvements

---
+++ js/catalog.js
@@ -0,0 +1,36 @@
+var cartPopup = document.querySelector(".cart-popup");
+var cartLink = document.querySelectorAll(".add-to-cart-button");
+var cartClose = document.querySelector(".close-modal-button");
+var bookmarkButton = document.querySelectorAll(".add-to-bookmarks-button");
+
+var bookmarkLink = document.querySelector(".header-bookmark");
+var orderLink = document.querySelector(".header-cart");
+
+for (var i = 0; i < cartLink.length; i++) {
+  cartLink[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+    orderLink.classList.add("item-added");
+  });
+}
+
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      cartPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+for (var i = 0; i < bookmarkButton.length; i++) {
+  bookmarkButton[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    bookmarkLink.classList.add("item-added");
+  });
+}
